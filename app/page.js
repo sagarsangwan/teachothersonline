5d8649ca44af6b0f39ed49be16390d36f7ec7c0e
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-03 15:42:07 +0530

    added a path for teacher application

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import { CustomEditor } from "@/components/editorjs/CustomEditor";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
-        <CustomEditor />
       </h1>
     </main>
   );
---
+++ app/teacher-application/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function teacherapplication() {
+    return (
+        <div>
+            <h1>Teacher Application</h1>
+        </div>
+    )
+}
+
+export default teacherapplication
