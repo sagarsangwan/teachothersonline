5c9b8080bf7360e8140083eedb145727d653683f
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-05 18:23:16 +0530

    we can now update teacher in our database

--- app/api/teacher-form-submission/route.js
+++ app/api/teacher-form-submission/route.js
@@ -1,27 +1,38 @@
 import { NextResponse } from "next/server"
 import { auth } from "@/auth"
+import prisma from "@/lib/prisma"
+import { date } from "zod"
 export async function POST(req, res) {
     const session = await auth()
     if (!session) {
         return NextResponse.unauthorized("Unauthorized")
     }
-
+    const existingTeacher = await prisma.TeacherApplication.findUnique({
+        where: {
+            userId: session.user.id
+        }
+    })
+    if (existingTeacher) {
+        return NextResponse.json({ message: "You have already submitted a teacher application", }, { status: 400 })
+    }
+    const { name, email } = await session.user
     const body = await req.json()
 
-    const { name } = body
-    // console.log(name , "inside api")
-    return NextResponse.json(name)
-    // const teacher = await prisma.teacher.create({
-    //     data: {
-    //         name,
-    //         email,
-    //         subjects: {
-    //             set: subjects
-    //         },
-    //         contact,
-    //         experience
-    //     }
-    // })
+    const { experience, contact, subjects, education } = body
+
+    const teacher = await prisma.TeacherApplication.create({
+        data: {
+            name: name,
+            resume: "",
+            experience: experience,
+            contact: contact,
+            subjects: subjects,
+            education: education,
+            userId: session.user.id,
+            verified: false
+        }
+    })
+    return NextResponse.json(teacher)
 
     // res.json(teacher)
 }
