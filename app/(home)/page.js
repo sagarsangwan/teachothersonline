7f606aa775a370e8b768a8acbf0d5fa5a33e5aa1
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-15 02:44:46 +0530

    admin cannot apply for teacher

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -4,8 +4,11 @@ export default async function Home() {
   const session = await auth()
   return (
     <main className="flex  flex-col">
+      {/* only show this if user role is not admin */}
+      {session.user.role !== "admin" && (
+        checkIsTeacherOrNot()
+      )}
 
-      {checkIsTeacherOrNot()}
 
     </main>
   );
