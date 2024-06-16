21433c311c5a22dcc3de278bf068d6a98ed928f8
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-17 01:15:10 +0530

    removed teacher table from admin/teahers

--- app/(admin)/admin-dashboard/teachers/page.js
+++ app/(admin)/admin-dashboard/teachers/page.js
@@ -1,23 +1,23 @@
 
-import { columns } from "./columns";
-import { DataTable } from "./data-table";
+// import { columns } from "./columns";
+// import { DataTable } from "./data-table";
 import prisma from "@/lib/prisma"
 async function page() {
-    const applicants = await prisma.Teacher.findMany(
-        {
-            include: {
-                user: true
-            },
-        }
-    )
-    // const initial 
+    // const applicants = await prisma.Teacher.findMany(
+    //     {
+    //         include: {
+    //             user: true
+    //         },
+    //     }
+    // )
+    // // const initial 
 
 
     return (
 
         <div>
             <h1>Teachers</h1>
-            <DataTable columns={columns} data={applicants} />
+            {/* <DataTable columns={columns} data={applicants} /> */}
         </div>
     )
 }
