6176feb655808a7083cc1b5c37cb8b97be362152
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-17 01:14:16 +0530

    ui fix: admin dashboard homepage

--- app/(admin)/admin-dashboard/page.js
+++ app/(admin)/admin-dashboard/page.js
@@ -1,8 +1,35 @@
-
-export default function page() {
+import { teacherColums } from "./teachers/columns"
+import { TeacherDataTable } from "./teachers/data-table"
+import allUserCount from "./_components/all-user-card"
+import allApplicantCount from "./_components/all-applicant-card"
+import allStudentCount from "./_components/all-student-card"
+export default async function page() {
+    try {
+        const applicants = await prisma.Teacher.findMany(
+            {
+                include: {
+                    user: true
+                },
+            }
+        )
+    } catch {
+        return null
+    } finally {
+        await prisma.$disconnect()
+    }
     return (
-        <div>
-            this is admin pannel-------------------------------------------------
+        <div className="flex flex-wrap gap-6 mt-8">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-3">
+                {allUserCount()}
+                {allApplicantCount()}
+                {allStudentCount()}
+            </div>
+            <div className="">
+                <div className="">
+                    <p className="text-lg mb-5">Teachers</p>
+                    <TeacherDataTable columns={teacherColums} data={applicants} />
+                </div>
+            </div>
         </div>
     )
 }
