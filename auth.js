810b3ac17f965c788b2faf0b1ec57918e741eddc
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-21 23:22:36 +0530

    removed extra components

--- auth.js
+++ auth.js
@@ -3,8 +3,23 @@ import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "./lib/prisma"
 
+const providers = [
+    Google
+]
+
+export const providerMap = providers.map((provider) => {
+    if (typeof provider === "function") {
+        const providerData = provider()
+        return { id: providerData.id, name: providerData.name }
+    } else {
+        return { id: provider.id, name: provider.name }
+    }
+})
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google],
+    providers,
+    // pages: {                             will do it later
+    //     signIn: "/signin",
+    // },
     adapter: PrismaAdapter(prisma),
     callbacks: {
 
