ffbcce254716ee89e072272c6198247a696a5764
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-25 19:12:34 +0530

    removed extra folder and added more condition on initial user check

--- components/teacher/initial-user-check.jsx
+++ components/teacher/initial-user-check.jsx
@@ -92,6 +92,7 @@ async function initialUserCheck() {
 
 
     if (session) {
+        console.log("session haiiiiiiiiiiiiiiiii", session.user.role)
         if (session.user.role === "teacher") {
             let teacher = null
             console.log("=[[[[[[[[[[[[[[[[=[[[[[[[[[[[[[")
@@ -109,7 +110,7 @@ async function initialUserCheck() {
             if (teacher) {
                 return (
                     <div>
-
+                        sagar
                     </div>)
             }
         }
@@ -153,30 +154,93 @@ async function initialUserCheck() {
                 </div >
             )
         }
+        if (session.user.role === "user" && teacherApplication) {
+            return (
+                (<div>
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>welcome {session.user.name} <Badge variant="green">pending</Badge>  </CardTitle>
+                            {/* <CardDescription>You have 3 unread messages.</CardDescription> */}
+                        </CardHeader>
+                        <CardContent className="grid gap-4">
+                            <p>
+                                Your application for teaching with subjects <span className=" font-bold"> {teacherApplication.subjects}</span> is pending. We will get back to you soon.
+                            </p>
+                        </CardContent>
+                    </Card>
+                </div>)
+
+            )
+        }
+        if (session.user.role === "user" && !teacherApplication && !demoClass) {
+
+            return (
+                <>
+                    <div className="flex flex-wrap md:h-screen">
+                        <div className="w-full my-16 sm:w-1/2  md:my-auto sm:px-6">
+                            <p className="flex flex-col space-y-4 md:space-y-7">
+                                <span className=" text-2xl md:text-5xl font-medium">Book a class</span>
+                                <span>Request a demo, start learning</span>
+                                <span>
+                                    {/* <Input />
+                                <Input />
+                                <Input /> */}
+                                    {/* <Button color="primary">
+                                        <Link href="/teacher-application">
+                                            Book a demo class
+                                        </Link>
+                                    </Button> */}
+                                </span>
+                            </p>
+                            <div className="sm:w-[238px] md:w-[324px]">
+                                <DemoClassStudent />
+                            </div>
+
+                        </div>
+                        <div className="sm:w-1/2 sm:my-auto ">
+
+                            <Image alt="" src={studentlearn} />
+
+                        </div>
+
+
+                    </div >
+                    <div className="flex flex-wrap md:h-screen">
+                        <div className=" my-16 sm:w-1/2 sm:my-auto ">
+
+                            <Image alt="" src={teacher} />
+
+                        </div>
+                        <div className="w-full sm:w-1/2 sm:my-16 md:my-auto">
+                            <p className="flex flex-col  space-y-4 md:space-y-7 text-center px-6">
+                                <span className=" text-3xl md:text-6xl font-medium">Teach when you want and earn</span>
+                                <span> Hi <span className=" font-bold"> {session ? session.user.name : "Guest"}</span> want some extra income by teaching?
+                                    Apply to be a teacher and start earning by teaching students.</span>
+                                <span>
+                                    <Button color="primary">
+                                        <Link href="/teacher-application">
+                                            Apply
+                                        </Link>
+                                    </Button>
+                                </span>
+                            </p>
+
+                        </div>
+
+
+
+                    </div >
+
+                </>
+            )
+        }
+
     }
 
 
 
 
 
-    else if (teacherApplication) {
-        return (
-            (<div>
-                <Card>
-                    <CardHeader>
-                        <CardTitle>welcome {session.user.name} <Badge variant="green">pending</Badge>  </CardTitle>
-                        {/* <CardDescription>You have 3 unread messages.</CardDescription> */}
-                    </CardHeader>
-                    <CardContent className="grid gap-4">
-                        <p>
-                            Your application for teaching with subjects <span className=" font-bold"> {teacherApplication.subjects}</span> is pending. We will get back to you soon.
-                        </p>
-                    </CardContent>
-                </Card>
-            </div>)
-
-        )
-    }
     else {
 
         return (
