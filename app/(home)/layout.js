6bbbfed4e45493bc8d5401e174e88e06e6a56b80
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-27 13:48:15 +0530

    added suppressHydrationWarning

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -15,9 +15,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
 
-      <body className={inter.className}>
+      <body className={inter.className} >
         <SessionProvider>
           <Providers>
             <ClientProvider>
