de6af9d5dc3902674b2a0f59ac690efbc358e07d
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-28 12:20:48 +0530

    added name in user meeting

--- app/(home)/meetings/[id]/MeetingPage.jsx
+++ app/(home)/meetings/[id]/MeetingPage.jsx
@@ -1,11 +1,14 @@
 "use client"
+import isAuth from '@/lib/isAuth';
 import { Button } from '@/components/ui/button';
 import Loader from '@/components/ui/Loader';
-import { CallControls, SpeakerLayout, StreamCall, StreamTheme, useStreamVideoClient } from '@stream-io/video-react-sdk';
-import React, { useState } from 'react'
 
+import { CallControls, SpeakerLayout, StreamCall, StreamTheme, useStreamVideoClient, useCallStateHooks } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react'
+import { CiUser } from 'react-icons/ci';
 function MeetingPage({ id }) {
     const [call, setCall] = useState(null);
+    const [showParticipants, setShowParticipants] = useState(false)
     const client = useStreamVideoClient()
     if (!client) {
         return (<Loader />)
@@ -27,12 +30,16 @@ function MeetingPage({ id }) {
             <StreamCall call={call}>
                 <StreamTheme className=''>
                     <SpeakerLayout />
-                    <CallControls />
-
+                    <div className='flex content-center justify-center'>
+                        <CallControls />
+                        <div className=' my-auto'>
+                            <Button variant="outline" className="ms-2" onClick={() => setShowParticipants((prev) => !prev)} > <CiUser /> </Button>
+                        </div>
+                    </div>
                 </StreamTheme>
             </StreamCall>
         </div>
     )
 }
 
-export default MeetingPage
+export default isAuth(MeetingPage)
--- components/providers/ClientProvider.jsx
+++ components/providers/ClientProvider.jsx
@@ -18,7 +18,7 @@ function ClientProvider({ children }) {
         if (session?.user?.id) {
             streamUser = {
                 id: session.user.id,
-                name: session.user.username,
+                name: session.user.name,
                 image: session.user.image
             }
         } else {
@@ -69,106 +69,6 @@ function ClientProvider({ children }) {
 
 }
 
-// function useIntilizeVideoClient() {
-//     const { data: session, status } = useSession()
-//     const [videoCLient, setVideoClient] = useState(null)
-//     useEffect(() => {
-//         if (status === "loading") return;
-//         let streamUser = "";
-//         if (session?.user?.id) {
-//             streamUser = {
-//                 id: session.user.id,
-//                 name: session.user.username,
-//                 image: session.user.image
-//             }
-//         } else {
-
-//         }
-//         const apiKey = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY;
-//         const client = new StreamVideoClient({
-//             apiKey,
-//             user: streamUser,
-//             tokenProvider: streamUser?.id ? getToken : undefined
-//         })
-//         setVideoClient(client)
-
-//         return () => {
-//             client.disconnectUser();
-//             setVideoClient(null);
-//         };
-//     }, [session, status])
-
-//     return videoCLient
-// }
 
 export default ClientProvider
 
-
-
-// import { useSession } from 'next-auth/react';
-// import {
-//     StreamCall,
-//     StreamVideo,
-//     StreamVideoClient
-// } from '@stream-io/video-react-sdk';
-// import { useEffect, useState } from 'react';
-// import { getToken } from './actions';
-
-
-
-// const ClientProvider = ({ children }) => {
-//     const apiKey = process.env.NEXT_PUBLIC_STREAM_VIDEO_API_KEY;
-//     const { data: session, status } = useSession()
-//     const [videoClient, setVideoClient] = useState(null)
-
-//     useEffect(() => {
-//         if (status === 'loading' || !session) return;
-//         if (!apiKey) throw new Error('API key not found')
-//         const client = new StreamVideoClient({
-//             apiKey,
-//             user: {
-//                 id: session.user.id,
-//                 name: session.user.username,
-//                 image: session.user.image
-//             },
-//             tokenProvider: getToken
-//         })
-//         setVideoClient(client)
-//         return () => {
-//             client.disconnectUser()
-//             setVideoClient(null)
-//         }
-
-
-//     }, [session, status, apiKey])
-//     if (status === "loading") {
-//         return (
-//             <div className="flex h-screen justify-center items-center ">
-//                 <Loader />
-//             </div>
-//         )
-//     }
-
-//     if (status === "unauthenticated") {
-//         return (
-//             <div>
-//                 {children}
-//             </div>
-//         )
-//     }
-//     if (!videoClient) {
-//         return (
-//             <div className="flex h-screen justify-center items-center ">
-//                 <Loader />
-//             </div>
-//         )
-//     }
-//     return (
-//         <StreamVideo client={videoClient}>
-//             {children}
-//         </StreamVideo>
-//     );
-// };
-
-
-// export default ClientProvider;
