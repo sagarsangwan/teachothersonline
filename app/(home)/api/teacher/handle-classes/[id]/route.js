93c4e23df27bbc7caebe32c27d16b399771ea5df
sagar sangwan <iamsagarsangwan@gmail.com>
2024-06-28 21:54:53 +0530

    added prisma

--- app/(home)/api/teacher/handle-classes/[id]/route.js
+++ app/(home)/api/teacher/handle-classes/[id]/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server"
 import { auth } from "@/auth"
+import prisma from "@/lib/prisma"
 export async function POST(req, { params }) {
     const classId = params.id
 
