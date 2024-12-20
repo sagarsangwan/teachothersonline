be57bd4906748018cf6a778539f32b1be98b7a88
sagar sangwan <iamsagarsangwan@gmail.com>
2024-12-20 12:34:24 +0530

    updated in nextjs 15

--- auth.js
+++ auth.js
@@ -1,16 +1,10 @@
-import NextAuth from "next-auth"
+// import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "./lib/prisma"
-
-
-
+import NextAuth from "next-auth"
+ import prisma from "./lib/prisma"
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
 
-    providers: [Google],
-    // pages: {                             will do it later
-    //     signIn: "/signin",
-    // },
-   
+  providers: [Google],
 })
