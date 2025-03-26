7aa02921ba5245c063db9044e49c847e2ff477bc
sagar sangwan <iamsagarsangwan@gmail.com>
2025-03-26 21:59:49 +0530

    ?

--- app/(home)/api/teacher/teacher-form-submission/route.js
+++ app/(home)/api/teacher/teacher-form-submission/route.js
@@ -1,52 +1,55 @@
-import { NextResponse } from "next/server"
-import { auth } from "@/auth"
-import prisma from "@/lib/prisma"
-
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
 
 export async function POST(req, res) {
-    const session = await auth()
-    if (!session) {
-        return NextResponse.unauthorized("Unauthorized")
-    }
-    const existingTeacher = await prisma.Teacher.findUnique({
-        where: {
-            userId: session.user.id
-        }
-    })
-    if (existingTeacher) {
-        return NextResponse.json({ message: "You have already submitted a teacher application", data: existingTeacher, status: 400 })
-    }
-    const { name } = session.user
+  const session = await auth();
+  if (!session) {
+    return NextResponse.unauthorized("Unauthorized");
+  }
+  const existingTeacher = await prisma.Teacher.findUnique({
+    where: {
+      userId: session.user.id,
+    },
+  });
+  if (existingTeacher) {
+    return NextResponse.json({
+      message: "You have already submitted a teacher application",
+      data: existingTeacher,
+      status: 400,
+    });
+  }
+  const { name } = session.user;
 
+  const Formdata = await req.formData();
+  const resume = Formdata.get("resume");
+  const education = Formdata.get("education");
+  const contact = Formdata.get("contact");
+  const subjects = Formdata.getAll("subjects");
+  const experience = Formdata.get("experience");
+  // const subjects = allsubjects.split(',');
 
-    const Formdata = await req.formData();
-    const resume = Formdata.get("resume")
-    const education = Formdata.get('education')
-    const contact = Formdata.get('contact')
-    const subjects = Formdata.getAll('subjects')
-    const experience = Formdata.get('experience')
-    // const subjects = allsubjects.split(',');
-
-    try {
-        const teacher = await prisma.Teacher.create({
-            data: {
-                name: name,
-                resume: "",
-                experience: experience,
-                contact: contact,
-                subjects: subjects,
-                education: education,
-                userId: session.user.id,
-                verified: false
-            }
-        })
-        return NextResponse.json({ message: " submitted successfully", data: teacher, status: 201 })
-    } catch (error) {
-        return NextResponse.internalServerError("Something went wrong")
-    } finally {
-        await prisma.$disconnect();
-    }
-
-
-
+  try {
+    const teacher = await prisma.Teacher.create({
+      data: {
+        name: name,
+        resume: "",
+        experience: experience,
+        contact: contact,
+        subjects: subjects,
+        education: education,
+        userId: session.user.id,
+        verified: false,
+      },
+    });
+    return NextResponse.json({
+      message: " submitted successfully",
+      data: teacher,
+      status: 201,
+    });
+  } catch (error) {
+    return NextResponse.internalServerError("Something went wrong");
+  } finally {
+    await prisma.$disconnect();
+  }
 }
--- auth.js
+++ auth.js
@@ -1,10 +1,18 @@
 // import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import NextAuth from "next-auth"
- import prisma from "./lib/prisma"
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import prisma from "./lib/prisma";
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
 
   providers: [Google],
-})
+  callbacks: {
+    async session({ session, user }) {
+      // Include extra fields in the session object
+      session.user.id = user.id;
+      session.user.role = user.role;
+      return session;
+    },
+  },
+});
